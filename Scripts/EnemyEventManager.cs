dbd8ce32d7ed2e5d582dee7efdd9d6f07b9b5669
McWenker <nlax76@gmail.com>
2018-12-14 18:23:28 -0500

    Improved AI pathfinding
    Added aggro range, line of sight, group aggro, and de-aggro mechanics.

---
+++ Scripts/EnemyEventManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EnemyEventManager
+{
+    public delegate void AggroActionEventHandler(GameObject sender, Transform target);
+    public static event AggroActionEventHandler OnAggro;
+
+    public static void NewAggro(GameObject sender, Transform targetTransform)
+    {
+        if (OnAggro != null) OnAggro(sender, targetTransform);
+    }
+}
